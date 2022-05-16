6f68e6d3b15bdd46fbfa7b49b407e66c1f0720a8
Ivan Cindric <ivan.cindric95@gmail.com>
2022-05-16 09:48:28 +0200

    iec 104 connect

--- iec104.py
+++ iec104.py
@@ -1,8 +1,10 @@
 import sys
+
 from hat import aio
-from hat.drivers.iec60870 import iec104
-from hat.drivers.iec60870 import apci
+import ssl
 from hat.drivers import tcp
+from hat.drivers.iec60870 import apci
+from hat.drivers.iec60870 import iec104
 
 
 def main():
@@ -10,11 +12,23 @@ def main():
 
 
 async def async_main():
-    link = await apci.connect(addr=tcp.Address('127.0.0.1', 1234))
+    ctx = ssl.SSLContext()
+    ctx.load_cert_chain(certfile='bundle.pem')
+    link = await apci.connect(addr=tcp.Address('192.168.0.191', 19998),
+                              ssl_ctx=ctx)
     conn = iec104.Connection(link)
-    print('nesto')
-    breakpoint()
-    pass
+    msg = iec104.InterrogationMsg(is_test=False,
+                                  originator_address=0,
+                                  asdu_address=1,
+                                  request=0,
+                                  is_negative_confirm=False,
+                                  cause=iec104.CommandReqCause.ACTIVATION)
+
+    conn.send([msg])
+    while True:
+        recv = await conn.receive()
+        print(recv)
+
     await conn.async_close()
 
 
