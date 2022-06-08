f577075bad09498b6d116b44f72177e8450642e8
1vanC1ndr1c <ivan.cindr95@gmail.com>
2022-06-08 16:09:14 +0200

    wip

--- journal_sanitizer.py
+++ journal_sanitizer.py
@@ -6,24 +6,25 @@ def main():
     with open('journal.txt', 'r') as in_file:
         in_file = [x.strip() for x in in_file.readlines()]
 
-    # remove everything but the file names
-    in_file = [x[x.index('|') + 1:] for x in in_file]
+    # split data
+    in_file = [x.split('|') for x in in_file]
+
+    # get only date, name, modification info
+    in_file = [(x[3], x[1], x[4]) for x in in_file]
 
     # find all created files
     print(f'CREATED {"=" * 100}')
-    created_files = [x for x in in_file if 'CREATE' in x]
-    # remove unnecessary data
-    created_files = [x[:x.index('|')] for x in created_files]
+    # format the output as "date - name"
+    created_files = [f'{x[0]} - {x[1]}' for x in in_file if 'CREATE' in x[2]]
     # remove duplicates but preserve order
     created_files = sorted(set([x for x in created_files]),
                            key=created_files.index)
-
     [print(x) for x in created_files]
     print(f'{"=" * 110}')
 
     # find all modified files
     print(f'MODIFIED {"=" * 100}')
-    modified = [x[:x.index('|')] for x in in_file]
+    modified = [f'{x[0]} - {x[1]}' for x in in_file]
     # for readability, don't include created files in modified files
     modified = [x for x in modified if x not in created_files]
     # remove duplicates but preserve order
